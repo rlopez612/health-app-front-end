a8b22f232f4b2585f9cf3c5228b3926d144c5234
Matt Moore <mmoore@catalyte.io>
2021-08-11 12:36:30 -0700

    Resolve conflicts from previous merge

--- src/components/reservation/Reservation.js
+++ src/components/reservation/Reservation.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import Form from '../form/Form';
 import Input from '../input/Input';
